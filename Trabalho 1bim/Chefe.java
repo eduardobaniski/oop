e49e96ec4fa93394bf1800fa9e9f012bf7d21f1a
Cadu <cadub6@gmail.com>
2023-05-11 14:15:35 -0300

    chefe

--- Trabalho 1bim/Chefe.java
+++ Trabalho 1bim/Chefe.java
@@ -1,4 +1,27 @@
 public class Chefe extends Funcionario {
     private String cargo;
-    
+
+    public Chefe(String nome, Data nascimento, String registro, double salario, String cargo) {
+        super(nome, nascimento, registro, salario);
+        this.cargo = cargo;
+    }
+
+    public Chefe(String nome, String telefone, Data nascimento, String registro, double salario, String cargo) {
+        super(nome, telefone, nascimento, registro, salario);
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public double bonificar(){
+        return this.salario * 0.2;
+    }
+    public double getSalarioComBonificacao(){
+        return salario + bonificar();
+    }
 }
